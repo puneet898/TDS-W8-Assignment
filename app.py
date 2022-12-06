f1d7615dc7f713f69656cc5eb1adc613632a61bb
puneet898 <114630695+puneet898@users.noreply.github.com>
2022-12-06 23:24:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ st.write("TDS Week 8 Assignment")
 a=st.number_input("Enter first number")
 b=st.number_input("Enter second number") 
 if (b == 0):
-  st.write("Infinity")
+  st.write("Division not possible.")
 else:
   c=a/b
 st.write("Division of the Two numbers is: ",c)
