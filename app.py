882c1ec702b54d71de050380ed327300ff54974e
puneet898 <114630695+puneet898@users.noreply.github.com>
2022-12-06 23:13:18 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+st.write("TDS Week 8 Assignment")
+a=st.number_input("Enter first number")
+b=st.number_input("Enter second number") 
+c=a/b
+st.write("Division of the Two numbers is: ",c)
+st.write("By Puneet 21f3002005")
