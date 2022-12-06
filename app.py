805883ea5a97c5d089b034e74e4567d0a990e7d7
puneet898 <114630695+puneet898@users.noreply.github.com>
2022-12-06 23:18:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import streamlit as st
 st.write("TDS Week 8 Assignment")
 a=st.number_input("Enter first number")
 b=st.number_input("Enter second number") 
-c=a/b
+if (b == 0):
+  st.write("Infinity")
+else:
+  c=a/b
 st.write("Division of the Two numbers is: ",c)
 st.write("By Puneet 21f3002005")
